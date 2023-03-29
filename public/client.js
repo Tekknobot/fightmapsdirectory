2bf95b0a24b56fff6e237f4e45957ad4e93583bb
Glitch (sqlite3-db) <none>
2023-03-29 07:01:31 +0000

    😋⛲️ Checkpoint ./public/client.js:56453687/36 ./views/index.html:56453687/21

--- public/client.js
+++ public/client.js
@@ -4,7 +4,7 @@
 $(function() {
   $.get('/users', function(users) {
     users.forEach(function(user) {
-      $('<li></li>').text(user[0] + " " + user[1] + " " + user[2] + " " + user[3]).appendTo('ul#users');
+      $('<li></li>').text(user[0] + " " + user[1] + " " + user[2]).appendTo('ul#users');
     });
   });
 
@@ -15,7 +15,7 @@ $(function() {
     var cityName = $('input#cityName').val();
     var countryName = $('input#countryName').val();    
     $.post('/users?' + $.param({machineName:machineName, streetAddress:streetAddress, cityName:cityName, countryName:countryName}), function() {
-      $('<li></li>').text(machineName + " " + cityName + " " + streetAddress + " " + countryName).appendTo('ul#users');
+      $('<li></li>').text(machineName + " " + cityName + " " + streetAddress).appendTo('ul#users');
       $('input#machineName').val('');
       $('input#streetAddress').val('');
       $('input#cityName').val('');
