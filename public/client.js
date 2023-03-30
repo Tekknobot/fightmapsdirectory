93314f2997ee664aeea5a3d412b736783de6a763
Glitch (fightmaps) <none>
2023-03-30 15:46:32 -0400

    push

--- public/client.js
+++ public/client.js
@@ -4,7 +4,7 @@
 $(function() {
   $.get('/users', function(users) {
     users.forEach(function(user) {
-      $('<tr><td></td></tr>').text(user[0] + " / " + user[1] + " / " + user[2]).appendTo('table#myTable');
+      $('<tr></tr>').text(user[0] + " / " + user[1] + " / " + user[2]).appendTo('table#myTable');
     });
   });
 
