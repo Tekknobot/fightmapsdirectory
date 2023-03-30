a754106920b67c0c76494f5d85e0195e8a33a2e6
Glitch (fightmaps) <none>
2023-03-30 16:00:33 -0400

    push

--- public/client.js
+++ public/client.js
@@ -4,6 +4,7 @@
 $(function() {
   $.get('/users', function(users) {
     users.forEach(function(user) {
+      $('<tr><td>' + user[0] + " / " + user[1] + " / " + user[2] + '</td></tr>').appendTo('ul#users');
       $('<tr><td>' + user[0] + " / " + user[1] + " / " + user[2] + '</td></tr>').appendTo('table#myTable');
     });
   });
--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/users", function (request, response) {
 }); 
 
 // creates a new entry in the users table with the submitted values
-app.post("/myTable", function (request, response) {
+app.post("/users", function (request, response) {
   User.create({ machineName: request.query.machineName, locationName: request.query.locationName, streetAddress: request.query.streetAddress , countryName: request.query.countryName});
   response.sendStatus(200);
 });
