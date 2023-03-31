9d73cfe99e28390bf8b43c1b318af4a69292adf9
Glitch (fightmaps) <none>
2023-03-31 14:42:42 -0400

    table button

--- server.js
+++ server.js
@@ -68,41 +68,6 @@ function setup(){
   );  
 }
 
-function sortTable() {
-  //get the parent table for convenience
-  let table = document.getElementById("myTable");
-
-  //1. get all rows
-  let rowsCollection = table.querySelectorAll("tr");
-
-  //2. convert to array
-  let rows = Array.from(rowsCollection)
-    .slice(1); //skip the header row
-
-  //3. shuffle
-  shuffleArray(rows);
-
-  //4. add back to the DOM
-  for (const row of rows) {
-    table.appendChild(row);
-  }
-}
-
-
-/**
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- * from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864#12646864
- */
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -119,7 +84,6 @@ app.get("/users", function (request, response) {
     });
     response.send(dbUsers); // sends dbUsers back to the page
   });
-  sortTable();
 }); 
 
 // creates a new entry in the users table with the submitted values
