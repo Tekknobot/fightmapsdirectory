a27e0479cf7d26b13b883811bfa29f6eb8caf9b0
Glitch (fightmaps) <none>
2023-03-31 14:26:41 -0400

    shuffle users db array

--- server.js
+++ server.js
@@ -68,6 +68,24 @@ function setup(){
   );  
 }
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -82,6 +100,7 @@ app.get("/users", function (request, response) {
     users.forEach(function(user) {
       dbUsers.push([user.machineName,user.locationName,user.streetAddress,user.countryName]); // adds their info to the dbUsers value
     });
+    shuffle(users);
     response.send(dbUsers); // sends dbUsers back to the page
   });
 }); 
